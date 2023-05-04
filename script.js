a59bf22a1570f7eca4efaa7565fdfd3804a56ef8
Анастасия <a.rangaeva1996@gmail.com>
2023-05-04 17:23:47 +0300

    Complete homework1

---
+++ script.js
@@ -0,0 +1,20 @@
+
+  
+//Домашнє завдання
+const number = prompt('Enter you age');
+alert(number);
+
+//2 
+const string1 = prompt('Enter you name');
+const string2 = prompt('Enter what city do you live in');
+const string3 = prompt('Enter what country do you live in');
+console.log('Hello, your name is ', string1,'.You live in, ',string2, ',',string3);
+
+
+
+
+
+//3
+console.log('Value: 156798; type: number');
+console.log('Value: IT School Hillel; type: string');
+console.log('Value: false; type: boolean');
